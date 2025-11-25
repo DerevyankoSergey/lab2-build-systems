b24db16fb39ee57ea89e80740e591e646092e310
DerevyankoSergey <serui8800555000@gmail.com>
2025-11-25 21:44:17 +0300

    Added Maven project

---
+++ my-app/src/main/java/shop/model/Customer.java
@@ -0,0 +1,20 @@
+package shop.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Customer {
+    private String name;
+    private List<Product> purchases = new ArrayList<>();
+
+    public Customer(String name) { this.name = name; }
+
+    public void addPurchase(Product p) { purchases.add(p); }
+
+    public double getTotalSpent() {
+        return purchases.stream().mapToDouble(Product::getPrice).sum();
+    }
+
+    public String getName() { return name; }
+    public List<Product> getPurchases() { return purchases; }
+}
